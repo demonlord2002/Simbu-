42691029736570cd756f654bde7fb24836c04f51
demonlord2002 <drdoom2003p@gmail.com>
2025-09-21 23:49:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ from pyrogram import filters
 import os
 
 class Config:
-    API_ID = "27079591"
-    API_HASH = "c81ae4c3dc026ea4bf49842a8ce4a5f9"
+    API_ID = "23559126"
+    API_HASH = "58347a441c011b1b9ee3367ea936dcc4"
     #TOKEN = "6521122303:AAGCO3XMjcA0SN5NAi1M0NpmbmMxEtwwYbg"
-    TOKEN = os.environ.get("TOKEN", None)
-    MONGO_URL = "mongodb+srv://vivek:1234567890@cluster0.c48d8ih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    TOKEN = "8289940670:AAGQ8Z49PV6wZ_sfY5JWl192wPWPWyeBDCk"
+    MONGO_URL = "mongodb+srv://rubesh08virat:rubesh08virat@cluster0.d33p1rm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     START_PIC = "https://files.catbox.moe/ppvvg0.jpg"
     SUDOERS = filters.user(["8143754205"])
